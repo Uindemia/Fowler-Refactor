52ae69be7d462e1d5033fad7fd852792402eeb77
Uindemia <marc@m-mahler.de>
2016-04-28 11:49:16 +0200

    some changes

--- src/Prog.java
+++ src/Prog.java
@@ -1,19 +1,7 @@
 /**
  * Created by Marc on 28.04.2016.
  */
-import java.lang.*;
-import java.util.*;
-/**
- * Note that Java console applications need to be run through the java runtime
- * by running "java -jar JarFile.jar" in the command line.
- * Java console applications can not be previewed in the Compilr IDE, only applets can.
- */
-public class Program
-{
-    /**
-     * This is the main entry point for the application
-     */
-
+public class Prog {
 
     public static void main(String args[])
     {
@@ -24,10 +12,11 @@ public class Program
         Rental r1 = new Rental(m1, 10);
         Rental r2 = new Rental(m2, 5);
         Customer c1 = new Customer("joe");
-        c1.addRental(r1);   c1.addRental(r2);
+        c1.addRental(r1);
+        c1.addRental(r2);
         System.out.println("Let's get the Statement");
         result = c1.statement();
         System.out.println(result);
     }
-}
 
+}
