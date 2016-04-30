37441c10383839801f76c3f8e836645f5662710f
Uindemia <marc@m-mahler.de>
2016-04-30 16:43:35 +0200

    extracted Query of FrequentRenterPoints and TotalCharge

--- src/Customer.java
+++ src/Customer.java
@@ -20,27 +20,42 @@ class Customer {
     };
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();	    
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
             Rental each = (Rental) enum_rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.amountFor()) + "\n";
-            totalAmount += each.amountFor();
+
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
     }
 
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rental = rentals.elements();
+        while (rental.hasMoreElements()) {
+            Rental each = (Rental) rental.nextElement();
+            result += each.amountFor();
+        }
+        return result;
+    }
 
+    private int getTotalFrequentRenterPoints(){
+        int result = 0;
+        Enumeration rental = rentals.elements();
+        while (rental.hasMoreElements()) {
+            Rental each = (Rental) rental.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
 
 }
     
